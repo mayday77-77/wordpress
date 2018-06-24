13b40d4ba2c893af10baa853c278e20cd6b1e846
ed.cheng <mayday77@gmail.com>
2018-06-24 17:12:10 +0800

    added files

--- installwp.sh
+++ installwp.sh
@@ -5,6 +5,7 @@
 # global variables
 WPfile=WP.tar.gz
 mysqlFile=mysqluser.sql
+saltFile=saltfile.txt
 
 # install all required software
 installAll()
@@ -30,17 +31,31 @@ installSQL()
    wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
    rpm -ivh mysql-community-release-el7-5.noarch.rpm
    yum -y install mysql-server
+   /usr/bin/mysql_safe &
    rm -f mysql-community-release-el7-5.noarch.rpm
    # create user
+   mysql -u root < $mysqlFile
 }
 
 # install wordpress
 installWP()
 ###########
 {
+   #install php7
+   yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+   yum -y install yum-utils
+   yum-config-manager --enable remi-php72 
+   yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo 
+   
+   # get latest wp files
    wget https://wordpress.org/latest.tar.gz -O $WPfile
    tar xfz $WPfile
    rm $WPfile
+
+   # retrieve salt values
+   wget https://api.wordpress.org/secret-key/1.1/salt/ -O $saltFile
+   # replace salt values
+   
 }
 
 # start the services
---
+++ mysqluser.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO "wp_user"@"localhost" IDENTIFIED BY "password";
+EXIT
