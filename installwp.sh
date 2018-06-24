c0d4b3ad5d34b01f8bb00f63eb35eb264505267a
ed.cheng <mayday77@gmail.com>
2018-06-24 20:17:27 +0800

    changes

--- installwp.sh
+++ installwp.sh
@@ -2,26 +2,23 @@
 # Description : install wordpress and start it
 ###############################################
 
-# global variables
-WPfile=WP.tar.gz
-mysqlFile=mysqluser.sql
-saltFile=saltfile.txt
 
-# install all required software
-installAll()
+updatePKG()
 ###########
 {
-   #yum update
-   installWebServer
-   installSQL
-   installWP
+   yum update
+   yum -y install wget
+   wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+   rpm -ivh epel-release-latest-7.noarch.rpm
+   rm -f epel-release-latest-7.noarch.rpm
 }
+
 
 # install apache webserver
 installWebServer()
 #################
 {
-   yum -y install wget httpd
+   yum -y install httpd
 }
 
 # install mqsql
@@ -31,41 +28,47 @@ installSQL()
    wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
    rpm -ivh mysql-community-release-el7-5.noarch.rpm
    yum -y install mysql-server
-   /usr/bin/mysql_safe &
+   /usr/bin/mysqld_safe &
    rm -f mysql-community-release-el7-5.noarch.rpm
    # create user
-   mysql -u root < $mysqlFile
+   mysql -u root < conf/mysqluser.sql
 }
 
 # install wordpress
 installWP()
 ###########
 {
-   #install php7
-   yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-   yum -y install yum-utils
-   yum-config-manager --enable remi-php72 
+   DB_NAME=wordpress
+   DB_USER=wp_user
+   DB_PASSWORD=password
+   #install php
+   #yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+   #yum -y install yum-utils
+   #yum-config-manager --enable remi-php72 
    yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo 
    
    # get latest wp files
-   wget https://wordpress.org/latest.tar.gz -O $WPfile
-   tar xfz $WPfile
-   rm $WPfile
-
+   wget https://wordpress.org/latest.tar.gz
+   tar xfz latest.tar.gz
+   dirName=`tar tf latest.tar.gz| head -1`
+   
+   # DB credentials in wp-config.php
+   cp $dirName/wp-config-sample.php $dirName/wp-config.php 
+   sed -i "s/database_name_here/$DB_NAME/" $dirName/wp-config.php
+   sed -i "s/username_here/$DB_USER/" $dirName/wp-config.php
+   sed -i "s/password_here/$DB_PASSWORD/" $dirName/wp-config.php
    # retrieve salt values
-   wget https://api.wordpress.org/secret-key/1.1/salt/ -O $saltFile
+   #wget https://api.wordpress.org/secret-key/1.1/salt/ -O conf/saltfile.txt
    # replace salt values
+   cp -rp $dirName /var/www/html
+   service httpd start 
    
 }
 
-# start the services
-startServices()
-###############
-{
-   service httpd start
-   service mysqld start
-}
 
 # main
-installAll
-#startService
+updatePKG
+installSQL
+installWebServer
+installWP
+
